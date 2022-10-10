a016c60440efeeab697df66e43d27172fb98de31
Huiyu Xie <xhyx0811@163.com>
2022-10-10 22:54:41 +0200

    xdcommit

--- concatenateImages.py
+++ concatenateImages.py
@@ -6,6 +6,8 @@ from PIL import Image, ImageDraw, ImageFont
 import matplotlib
 import matplotlib.pyplot as plt
 from pathlib import Path
+from sympy import symbols, solve
+from numpy import ceil
 
 matplotlib.use('TkAgg')
 
@@ -21,9 +23,7 @@ def words_to_picture(font, word, font_colour, bkg_src, size_limit):
     :return: Picture which stacked together
     :return: Change Colour pictures
     """
-    full_punc = 0
-    blank_area_t = 0
-    blank_area_b = 0
+
     word = word
     fontname = font.split(".")[-2]
     word_capital_array = []
@@ -31,11 +31,10 @@ def words_to_picture(font, word, font_colour, bkg_src, size_limit):
     word_array = []
     picture_array = []
     width_array = []
-    stack = np.ones([128, 0, 3]) * 255
 
-    def split_word(word):
-        for w in word:
-            print(w, fontname)
+    def split_word(word_str):
+        print(">>CON<< Word: ", word_str, " | ", fontname, " | ", len(word_str))
+        for w in word_str:
             word_array.append(w)
             if w == " ":
                 a = np.ones([128, 32, 3]) * 255
@@ -56,75 +55,175 @@ def words_to_picture(font, word, font_colour, bkg_src, size_limit):
 
     split_word(word)
 
-    i = 0
-    for pic in picture_array:
-        width = pic.shape[1]
-        height = pic.shape[0]
-        width_array.append(width)
-        if height >= 128:
-            height_loss = 0
-            blank_area_t = np.ones([128 - height - height_loss, width, 3]) * 255
-            blank_area_b = np.ones([height_loss, width, 3]) * 255
-        elif word[i] == "p" or word[i] == "q" or word[i] == "g":
-            o_img = cv2.imread("final/{}".format(fontname + "/low/" + "o" + "_" + fontname + ".png"))
-            margin = 128 - o_img.shape[0] - 25
-            height_loss = 128 - margin - o_img.shape[0]
-            # print(margin)
-            # print(height_loss)
-            # print(margin + height_loss)
-            blank_area_t = np.ones([margin, width, 3]) * 255
-            blank_area_b = np.ones([128 - height - margin, width, 3]) * 255
-        elif word[i] in string.punctuation:
-            if width < 32:
-                margin_l = round(32 - width / 2)
-                margin_r = 32 - margin_l
-                blank_area_l = np.ones([height, margin_l, 3]) * 255
-                blank_area_r = np.ones([height, margin_r, 3]) * 255
-                pic = np.hstack((blank_area_l, pic, blank_area_r))
-                height = pic.shape[0]
-                width = pic.shape[1]
-                height_loss = 15
-                blank_area_t = np.ones([128 - height - height_loss, width, 3]) * 255
-                blank_area_b = np.ones([height_loss, width, 3]) * 255
-            elif height < 110:
-                height_loss = 15
-                blank_area_t = np.ones([128 - height - height_loss, width, 3]) * 255
-                blank_area_b = np.ones([height_loss, width, 3]) * 255
-            else:
+    def pic_process(array, txt_array):
+        global after_stack
+        after_stack = np.ones([128, 0, 3]) * 255
+        stack = np.ones([128, 0, 3]) * 255
+        i = 0
+        # stack = np.ones([128, 0, 3]) * 255
+        for pic in array:
+            if pic is None:
+                print(txt_array[i])
+            width = pic.shape[1]
+            height = pic.shape[0]
+            width_array.append(width)
+            if height >= 120:
                 height_loss = 0
                 blank_area_t = np.ones([128 - height - height_loss, width, 3]) * 255
                 blank_area_b = np.ones([height_loss, width, 3]) * 255
-        else:
-            height_loss = 25
-            blank_area_t = np.ones([128 - height - height_loss, width, 3]) * 255
-            blank_area_b = np.ones([height_loss, width, 3]) * 255
-        # print("T/P/B: " + word[i] + " | " + str(blank_area_t.shape[0]) + " | " + str(height) + " | " + str(
-        #     blank_area_b.shape[0]))
-        full = np.vstack((blank_area_t, pic, blank_area_b))
-        # print("Full: " + str(full.shape))
-        pre_stack = np.hstack((stack, full))
-        if pre_stack.shape[1] > size_limit:
-            print("Exceeds")
-            remaining_array = picture_array[i:len(picture_array) + 1]
-            break
-        else:
+            elif txt_array[i] == "p" or txt_array[i] == "q" or txt_array[i] == "g" or txt_array[i] == "y":
+                o_img = cv2.imread("final/{}".format(fontname + "/low/" + "o" + "_" + fontname + ".png"))
+                margin = 128 - o_img.shape[0] - 25
+                blank_area_t = np.ones([margin, width, 3]) * 255
+                blank_area_b = np.ones([128 - height - margin, width, 3]) * 255
+            elif txt_array[i] in string.punctuation:
+                if width < 32:
+                    # margin_l = round(32 - width / 2)
+                    # margin_r = 32 - margin_l
+                    # blank_area_l = np.ones([height, margin_l, 3]) * 255
+                    # blank_area_r = np.ones([height, margin_r, 3]) * 255
+                    # pic = np.hstack((blank_area_l, pic, blank_area_r))
+                    height = pic.shape[0]
+                    width = pic.shape[1]
+                    height_loss = 15
+                    blank_area_t = np.ones([128 - height - height_loss, width, 3]) * 255
+                    blank_area_b = np.ones([height_loss, width, 3]) * 255
+                elif height < 110:
+                    height_loss = 15
+                    blank_area_t = np.ones([128 - height - height_loss, width, 3]) * 255
+                    blank_area_b = np.ones([height_loss, width, 3]) * 255
+                else:
+                    height_loss = 0
+                    blank_area_t = np.ones([128 - height - height_loss, width, 3]) * 255
+                    blank_area_b = np.ones([height_loss, width, 3]) * 255
+            else:
+                height_loss = 25
+                if height + height_loss > 128:
+                    height_loss = 128 - height
+                blank_area_t = np.ones([128 - height - height_loss, width, 3]) * 255
+                blank_area_b = np.ones([height_loss, width, 3]) * 255
+            full = np.vstack((blank_area_t, pic, blank_area_b))
             stack = np.hstack((stack, full))
-        i = i + 1
+            # print("OG_Stack: ", stack.shape[1])
+            i = i + 1
+        return stack
 
-    change_colour = Image.fromarray(stack.astype('uint8'), 'RGB')
-    width, height = change_colour.size
-    bkg_src_p = Image.open(bkg_src)
-    for w in range(width):
-        for h in range(height):
-            current_color = change_colour.getpixel((w, h))
-            if current_color == (255, 255, 255):
-                # change_colour.putpixel((w, h), bkg_colour)
-                b_current_color = bkg_src_p.getpixel((w, h))
-                change_colour.putpixel((w, h), b_current_color)
-                # print("CUR: BACK")
-            elif current_color == (0, 0, 0):
-                # print("CUR: FONT")
-                change_colour.putpixel((w, h), font_colour)
-    cv2.imshow('image', stack)
-    cv2.waitKey(0)
-    return stack, change_colour
+    def v_page_stack(array):
+        full_array = []
+        full_pic = np.ones([0, size_limit, 3]) * 255
+        for ele in array:
+            if ele.shape[1] < size_limit:
+                width_loss = size_limit - ele.shape[1]
+                width_patch = np.ones([ele.shape[0], width_loss, 3]) * 255
+                full = np.hstack((ele, width_patch))
+                full_array.append(full)
+            else:
+                cropped = ele[0:ele.shape[0], 0:size_limit]
+                print(cropped.shape)
+                full_array.append(cropped)
+        for fs in full_array:
+            full_pic = np.vstack((full_pic, fs))
+        cv2.imshow("full.png", full_pic)
+        cv2.waitKey(0)
+
+    # cv2.imshow('image', final_stack)
+    # cv2.waitKey(0)
+    def multi_init(stack):
+        print("size limit : ", size_limit)
+        size_multiplier = ceil(stack.shape[1] / size_limit)
+        indicators = []
+        sum = 0
+        fsum = 0
+        i = 0
+        for pic in picture_array:
+            p_wid = pic.shape[1]
+
+            if sum + p_wid > size_limit:
+                indicators.append(i)
+                sum -= sum
+            sum += p_wid
+            fsum += p_wid
+            i += 1
+        f = len(picture_array)
+        z = len(indicators)
+        i = 0
+        sep_array = []
+        sep_wod_array = []
+        for ind in indicators:
+            if i == 0:
+                # print("sep_array: ", 0, ind)
+                ind_arr = picture_array[0:ind]
+                sp_word = word[0:ind]
+                sep_array.append(ind_arr)
+                sep_wod_array.append(sp_word)
+                i += 1
+            else:
+                # print("sep_array: ", indicators[i - 1], ind)
+                ind_arr = picture_array[indicators[i - 1]:ind]
+                sp_word = word[indicators[i - 1]:ind]
+                sep_array.append(ind_arr)
+                sep_wod_array.append(sp_word)
+                i += 1
+        # print("sep_array: ", indicators[z - 1], f - 1)
+        ind_arr = picture_array[indicators[z - 1]:f - 1]
+        sep_array.append(ind_arr)
+        sp_word = word[indicators[z - 1]:f - 1]
+        sep_wod_array.append(sp_word)
+
+        print("len: sep_array: ", len(sep_array))
+        print("indicators: ", indicators)
+        print("stack_sum: ", fsum, stack.shape[1])
+        print("size_multiplier: ", size_multiplier)
+        print("words: ", sep_wod_array)
+
+        final_pic_array = []
+        i = 0
+        for sep in sep_array:
+            pic = pic_process(sep, sep_wod_array[i])
+            print("Shape: ", pic.shape[1])
+            final_pic_array.append(pic)
+            cv2.imshow("pic", pic)
+            cv2.waitKey(0)
+            i += 1
+        v_page_stack(final_pic_array)
+
+    def apply_bkg(stack):
+        change_colour = Image.fromarray(stack.astype('uint8'), 'RGB')
+        cg_width, cg_height = change_colour.size
+        bkg_src_p = Image.open(bkg_src)
+        for wd in range(cg_width):
+            for hd in range(cg_height):
+                current_color = change_colour.getpixel((wd, hd))
+                if current_color == (255, 255, 255):
+                    b_current_color = bkg_src_p.getpixel((wd, hd))
+                    change_colour.putpixel((wd, hd), b_current_color)
+                elif current_color == (0, 0, 0):
+                    change_colour.putpixel((wd, hd), font_colour)
+        colour = cv2.cvtColor(np.array(change_colour), cv2.COLOR_RGB2BGR)
+        cv2.imshow("pic", colour)
+        cv2.waitKey(0)
+        return change_colour
+
+    # v_page_array = []
+    # v_page_array.append(apply_bkg(final_stack))
+    # v_page_array.append(apply_bkg(after_stack))
+
+    final_stack = pic_process(picture_array, word)
+    multi_init(final_stack)
+    # apply_bkg(final_stack)
+
+    # v_page_stack(v_page_array)
+    # pre_stack = np.hstack((stack, full))
+    # if pre_stack.shape[1] > size_limit:
+    #     print("Exceeds")
+    #     size_multiplier = ceil(pre_stack.shape[1]/size_limit)
+    #     print(size_multiplier)
+    #     remaining_array = picture_array[i:len(picture_array) + 1]
+    #     dup = word[0:i]
+    #     text_array = word[i:len(picture_array) + 1]
+    #     print(" | ", dup, " | ", text_array, " | ")
+    #     after_stack = pic_process(remaining_array, text_array)
+    #     # cv2.imshow('image', after_stack)
+    #     # cv2.waitKey(0)
+    #     break
+    # else:
