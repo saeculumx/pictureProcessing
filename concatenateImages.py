735b4ce5c33ed71b1f29e14735d52ffd7e557d29
saeculumx <58507127+saeculumx@users.noreply.github.com>
2022-10-10 15:02:39 +0200

    back

--- concatenateImages.py
+++ concatenateImages.py
@@ -10,12 +10,16 @@ from pathlib import Path
 matplotlib.use('TkAgg')
 
 
-def words_to_picture(font, word):
+def words_to_picture(font, word, font_colour, bkg_src, size_limit):
     """
-    Make words into picture
+    Make words into picture, fully auto
+    :param size_limit: Size limit for the picture
+    :param bkg_src:  background src for pic
+    :param font_colour: font colour
     :param font: font
     :param word: String word
-    :return: Picture
+    :return: Picture which stacked together
+    :return: Change Colour pictures
     """
     full_punc = 0
     blank_area_t = 0
@@ -61,9 +65,9 @@ def words_to_picture(font, word):
             o_img = cv2.imread("final/{}".format(fontname + "/low/" + "o" + "_" + fontname + ".png"))
             margin = 128 - o_img.shape[0] - 25
             height_loss = 128 - margin - o_img.shape[0]
-            print(margin)
-            print(height_loss)
-            print(margin + height_loss)
+            # print(margin)
+            # print(height_loss)
+            # print(margin + height_loss)
             blank_area_t = np.ones([margin, width, 3]) * 255
             blank_area_b = np.ones([128 - height - margin, width, 3]) * 255
         elif word[i] in string.punctuation:
@@ -78,7 +82,7 @@ def words_to_picture(font, word):
                 height_loss = 15
                 blank_area_t = np.ones([128 - height - height_loss, width, 3]) * 255
                 blank_area_b = np.ones([height_loss, width, 3]) * 255
-            elif height<110:
+            elif height < 110:
                 height_loss = 15
                 blank_area_t = np.ones([128 - height - height_loss, width, 3]) * 255
                 blank_area_b = np.ones([height_loss, width, 3]) * 255
@@ -90,12 +94,35 @@ def words_to_picture(font, word):
             height_loss = 25
             blank_area_t = np.ones([128 - height - height_loss, width, 3]) * 255
             blank_area_b = np.ones([height_loss, width, 3]) * 255
-        print("T/P/B: " + word[i] + " | " + str(blank_area_t.shape[0]) + " | " + str(height) + " | " + str(
-            blank_area_b.shape[0]))
+        # print("T/P/B: " + word[i] + " | " + str(blank_area_t.shape[0]) + " | " + str(height) + " | " + str(
+        #     blank_area_b.shape[0]))
         full = np.vstack((blank_area_t, pic, blank_area_b))
-        print("Full: " + str(full.shape))
-        stack = np.hstack((stack, full))
+        # print("Full: " + str(full.shape))
+        pre_stack = np.hstack((stack, full))
+        if pre_stack.shape[1]>size_limit:
+            print("Exceeds")
+            # stack = np.hstack((stack, full))
+            remaining_array = np.array_split(np.array(picture_array),i,axis=0)
+            print(len(remaining_array))
+            break
+        else:
+            stack = np.hstack((stack, full))
         i = i + 1
 
+    change_colour = Image.fromarray(stack.astype('uint8'), 'RGB')
+    width, height = change_colour.size
+    bkg_src_p = Image.open(bkg_src)
+    for w in range(width):
+        for h in range(height):
+            current_color = change_colour.getpixel((w, h))
+            if current_color == (255, 255, 255):
+                # change_colour.putpixel((w, h), bkg_colour)
+                b_current_color = bkg_src_p.getpixel((w, h))
+                change_colour.putpixel((w, h), b_current_color)
+                # print("CUR: BACK")
+            elif current_color == (0, 0, 0):
+                # print("CUR: FONT")
+                change_colour.putpixel((w, h), font_colour)
     cv2.imshow('image', stack)
     cv2.waitKey(0)
+    return stack, change_colour
--- main.py
+++ main.py
@@ -9,9 +9,8 @@ from string import ascii_uppercase as ulc
 if __name__ == '__main__':
     filetype = ".png"
     font = "arial.ttf"
-
-
-    # concatenateImages.words_to_picture(font, "This is a: *[Example]* ,.?")
+    font_colour = (255, 255, 255)
+    bkg_colour = (42, 56, 67)
 
     def generate_cut():
         for punc in string.punctuation:
@@ -23,6 +22,7 @@ if __name__ == '__main__':
             print(filename)
             fileGenerator.generate_pics(128, 128, text, fontname, 90, capital="capital")
             importPics.img_bw("generate/{}".format("punc" + "_capital" + "/" + filename), capital="capital")
+
         for i in ulc:
             text = i
             fontname = font.split(".")[-2]
@@ -38,5 +38,6 @@ if __name__ == '__main__':
             importPics.img_bw("generate/{}".format(font + "_low" + "/" + filename), capital="low")
 
 
-    #generate_cut()
-    concatenateImages.words_to_picture(font, "This is a:\n *[Example]* ,.?")
+    # generate_cut()
+
+    concatenateImages.words_to_picture(font, "This is a: *[Example]* ,.?", font_colour, "bkg.png", 800)
